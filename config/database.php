639ad9f87237a5ee626e1a53fa4cfa027fc2d767
aguiot-- <aguiot--@student.42.fr>
2019-05-03 13:21:19 +0200

    Move to Docker

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$DB_NAME		= 'camagru';
-$DB_HOST		= '127.0.0.1';
-$DB_DSN			= "mysql;dbname=$DB_NAME;host=$DB_HOST";
-$DB_USER		= 'camagru';
-$DB_PASSWORD	= 'grumaca';
-
-?>
---
+++ www/config/setup.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('database.php');
+
+try {
+    $db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Échec lors de la connexion : ' . $e->getMessage();
+}
+
+?>
