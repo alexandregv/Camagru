3814160c3d1af9641477b865f5fa820132b54b4c
aguiot-- <aguiot--@student.42.fr>
2019-07-31 18:45:53 +0200

    A bit of CSS for /new

--- www/App/Views/Posts/new.php
+++ www/App/Views/Posts/new.php
@@ -6,12 +6,34 @@ Helpers::partial('navbar');
 ?>
 <main>
   <section class="section">
-	<form method="POST" action="<?= Helpers::route('Posts#new') ?>" enctype="multipart/form-data">
-	  <input type="file" id="picture" name="picture" accept="image/png, image/jpeg" onchange="readURL(this);">
-	  <input type="text" name="description">
-	  <input type="submit">
-	<form>
-	<img id="img" src="#"/>
+	<div class="box">
+      <h3 class="title is-3">Nouvelle publication</h3>
+	  <hr>
+
+	  <div class="columns">
+	  <div class="column">
+   	    <form method="POST" action="<?= Helpers::route('Posts#new') ?>" enctype="multipart/form-data">
+	      <label for="picture">Image</label>
+          <div class="file">
+            <label class="file-label">
+              <input class="file-input" type="file" id="picture" name="picture" accept="image/png, image/jpeg" onchange="readURL(this);">
+              <span class="file-cta">
+                <span class="file-icon"><i class="fas fa-upload"></i></span>
+                <span class="file-label">Choose a file</span>
+              </span>
+            </label>
+          </div>
+	      <br>
+	      <label for="description">Description</label>
+		  <textarea class="textarea" rows="7" name="description" placeholder="Ajoutez une breve description..."></textarea>
+	      <br>
+   	      <input type="submit" class="button is-primary">
+   	    <form>
+	  </div>
+	  <div class="column">
+   	    <img id="img" src=""/>
+      </div>
+    </div>
   </section>
 </main>
 <script type="text/javascript">
