eb92cd673887cf3fba6947bfd21aca2f0f35787e
aguiot-- <aguiot--@student.42.fr>
2019-08-05 14:36:08 +0200

    Add HTTP_REFERER check to CSRF middleware

--- www/App/Middlewares/CsrfMiddleware.php
+++ www/App/Middlewares/CsrfMiddleware.php
@@ -7,16 +7,14 @@ class CsrfMiddleware extends Middleware
 {
 	public function call(string &$body)
 	{
-		//if ($body == '') // Before
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') // Before
 		{
-			if (!isset($_SESSION['csrf']) || !isset($_POST['csrf']) || $_POST['csrf'] != $_SESSION['csrf'])
+			if (!isset($_SERVER['HTTP_REFERER']) || explode('/', $_SERVER['HTTP_REFERER'])[2] != 'localhost:8080' || !isset($_SESSION['csrf']) || !isset($_POST['csrf']) || $_POST['csrf'] != $_SESSION['csrf'])
 			{
 				Helpers::flash('danger', 'Ça joue les hackers ?');
 				return $this->router->redirect('Pages#home');
 			}
 		}
-		//else // After
 		else if ($_SERVER['REQUEST_METHOD'] == 'GET') // After
 		{
 			$csrf = '<input name="csrf" type="text" value="' . (isset($_SESSION['csrf']) ? $_SESSION['csrf'] : '') . '" hidden>';
