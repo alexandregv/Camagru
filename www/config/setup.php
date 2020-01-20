2ac68bcb5077111313e4d669303c7b8e0c319886
aguiot-- <aguiot--@student.42.fr>
2020-01-20 13:13:09 +0100

    Fix + clear setup.php

--- www/config/setup.php
+++ www/config/setup.php
@@ -2,52 +2,18 @@
 
 require_once 'database.php';
 
-//$DB_OPTIONS = [
-//  PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
-//  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
-//  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
-//];
-
 try {
     $db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD, $DB_OPTIONS);
 } catch (PDOException $e) {
-	die('Échec lors de la connexion: ' . $e->getMessage() . PHP_EOL);
+	if ($e->getCode() == 1049) {
+		try {
+			$db = new PDO("mysql:host=$DB_HOST", $DB_USER, $DB_PASSWORD, $DB_OPTIONS);
+		} catch (PDOException $e) {
+			die('Échec lors de la connexion: ' . $e->getMessage() . PHP_EOL);
+		}
+	}
+	else die('Échec lors de la connexion: ' . $e->getMessage() . PHP_EOL);
 }
-
-//$stmt = $db->prepare('SELECT * FROM test WHERE id = :id;');
-//$stmt->execute([':id' => 1]);
-//$stmt = $db->prepare('SELECT * FROM test WHERE id = :id;')->execute([':id' => 1]);
-//$rows = $stmt->fetchAll();
-//foreach ($rows as $row)
-//	echo '<pre>', print_r($row), '</pre>';
-
-//$db->query('DROP TABLE IF EXISTS `users`;');
-//$db->query(<<<SQL
-//	CREATE TABLE `users` (
-//		`id` INT NOT NULL AUTO_INCREMENT,
-//		`username` VARCHAR(32) NOT NULL,
-//		`first_name` VARCHAR(255) NOT NULL,
-//		`last_name` VARCHAR(255) NOT NULL,
-//		`mail` VARCHAR(255) NOT NULL,
-//		`passhash` TEXT NOT NULL,
-//		`mail_token` TEXT NOT NULL,
-//		PRIMARY KEY (`id`))
-//	ENGINE = InnoDB;
-//SQL
-//);
-//
-//$db->query('DROP TABLE IF EXISTS `photos`;');
-//$db->query(<<<SQL
-//	CREATE TABLE `photos` (
-//		`id` INT NOT NULL AUTO_INCREMENT,
-//		`title` VARCHAR(255) NOT NULL,
-//		`likes_count` INT NOT NULL, `comments_count` INT NOT NULL,
-//		`path` TEXT NOT NULL,
-//		`creator_id` INT NOT NULL,
-//		PRIMARY KEY (`id`))
-//	ENGINE = InnoDB;
-//SQL
-//);
 
 $sql = file_get_contents('setup.sql');
 $qr = $db->exec($sql);
