c23f2716f57f4271dd77a1dad58fdc9c8c8f6ff6
alexandregv <contact@alexandregv.fr>
2020-01-27 15:34:18 +0100

    Fix memory limit problem

--- www/public/index.php
+++ www/public/index.php
@@ -5,6 +5,7 @@ set_include_path('/var/www/html/');
 date_default_timezone_set('Europe/Paris');
 setlocale(LC_TIME, 'fr_FR.utf8');
 setlocale(LC_ALL,  'fr_FR.utf8');
+ini_set('memory_limit', '-1');
 
 session_start();
 require_once 'App/Autoloader.php';
